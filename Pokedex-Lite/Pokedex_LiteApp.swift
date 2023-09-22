fcbbd611f1f56be195c2bac655c52e4adada467e
Zaheer <zaheer-dvt@paystack.com>
2023-09-22 19:48:29 +0200

    Initial commit

---
+++ Pokedex-Lite/Pokedex_LiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokedex_LiteApp.swift
+//  Pokedex-Lite
+//
+//  Created by Zaheer Moola on 2023/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Pokedex_LiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
